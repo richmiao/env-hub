42bee058ac2e98857311e1b0d88a5129ebec3a63
Jeremy Bao <jeremy.bao@pwc.com>
2018-11-21 16:53:08 +0800

    use node to replace go to implment the Parameter center service

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# param-hub
+# Parameter Center
+
+
+### Install
+```shell
+$ npm i
+$ npm start
+```
+### How to use
+1. put your project level config file in `/config`
+2. renew the config info with `curl http://localhost:8888/api/v1/refresh`
+3. request the config info with `curl http://localhost:8888/api/v1/parameters/:path`
+
+For example, if you have a project called `XXX`, there are 3 types of config files for 3 different enviroments: `dev.json`, `uat.json`, `prod.json`. you can just put these files like
+```
+        --config
+            |
+            |--XXX
+                |
+                |--dev.json
+                |--prod.json
+                |--uat.json
+```
+If you want to access the config for XXX project's dev configure, you just need to `curl http://localhost:8888/api/v1/parameters/config.XXX.dev.json`
+
+          
---
+++ main.js
@@ -0,0 +1,69 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')()
+const fs = require("fs");
+const readdirp = require('readdirp');
+const { parse } = require('dotenv');
+
+var settings = {
+    root: './config',
+    fileFilter: [ '*.env', '*.json' ],
+    directoryFilter: [ '!.git', '!*modules' ],
+    entryType: 'files',
+    depth: 5
+};
+
+function load() {
+    config = {};
+    readdirp(settings,
+        // This callback is executed everytime a file or directory is found inside the providen path
+        function(fileInfo) {
+            const key = fileInfo.path.replace(/\//g, '.');
+            const content = fs.readFileSync(fileInfo.fullPath);
+            if(fileInfo.name.endsWith('.json')){
+                config[key] = JSON.parse(content);
+            }else if(fileInfo.name.endsWith('.env')) {
+                config[key.replace('.env', '.json')] = parse(content);
+            }
+        }, 
+    
+        function (err, res) {
+            if(err){
+                fastify.log.error("Error happened when loop the file system");
+                throw err;
+            }
+              
+        }
+    );
+}
+
+load();
+
+
+// Declare a route for get config
+fastify.get('/api/v1/parameters/:path', async (request, reply) => {
+    fastify.log.info(request.params.path);
+    return config[request.params.path]
+  })
+
+// Declare a route for refresh the load info
+fastify.get('/api/v1/refresh', async (request, reply) => {
+    load();
+    return "The config info is refreshed !";
+})
+
+// Declare a route for heath check
+fastify.get('/api/v1/ping', async (request, reply) => {
+    return "pong";
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(8888)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
